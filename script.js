9069a0b7e8e09cb41c669c9e88c4ea597c7e231b
Rupeshhh11 <rupesh.69.ff@icloud.com>
2026-04-02 12:22:06 +0530

    Add JavaScript logic for trailing outline

---
+++ script.js
@@ -0,0 +1,31 @@
+
+const cursorDot = document.querySelector('.cursor-dot');
+const cursorOutline = document.querySelector('.cursor-outline');
+
+window.addEventListener('mousemove', (r) => {
+    const posX = r.clientX;
+    const posY = r.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    cursorOutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`
+    }, { duration: 500, fill: "forwards" });
+});
+
+// Cursor hover
+document.querySelectorAll('button').forEach(el => {
+    el.addEventListener('mouseenter', () => {
+        cursorOutline.style.width = '60px';
+        cursorOutline.style.height = '60px';
+        cursorOutline.style.backgroundColor = 'rgba(255, 61, 0, 0.1)';
+    });
+
+    el.addEventListener('mouseleave', () => {
+        cursorOutline.style.width = '40px';
+        cursorOutline.style.height = '40px';
+        cursorOutline.style.backgroundColor = 'transparent';
+    });
+});
